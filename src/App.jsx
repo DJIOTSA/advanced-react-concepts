b3dccbd1e642a4e459361c97a871241caf4b8b88
DJIOTSA <mhulodjiotsa.com>
2025-01-25 11:32:56 +0100

    setting up the react with vite

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './assets/images/logo.svg'
+
+function App() {
+  return (
+    <>
+    <img style={{width:"50px", height:"auto"}} src={logo} alt="logo" />
+      <h1>Legal First Project</h1>
+      <h2>By Djiotsa Djouake Christian Daryn</h2>
+      <p>
+        The purpose of this document is to define the functional and
+        non-functional requirements for the Legal First Aid platform. The
+        platform aims to educate citizens about their legal rights under the
+        Cameroon Penal Code, answer frequently asked legal questions, and
+        connect users with legal experts.
+      </p>
+    </>
+  );
+}
+
+export default App;
